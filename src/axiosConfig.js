69d5c0df5f36d5e6e03b6b682e32bda379478ea7
fiyorig <fiyojof21@gmail.com>
2024-06-26 00:16:20 -0500

    logout

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosBase = axios.create({
-    baseURL: 'http://localhost:5500/api',
-    // baseURL:'https://evanforem-backend-deploy-1.onrender.com/api',
+    // baseURL: 'http://localhost:5500/api',
+    baseURL:'https://evanforem-backend-deploy-1.onrender.com/api',
     })
 
 export default axiosBase
